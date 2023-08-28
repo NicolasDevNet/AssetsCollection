43b9e6cd8735a48cdf728e2d6c9166ff00362b91
Alphadot <126687105+NicolasDevNet@users.noreply.github.com>
2023-08-28 14:37:32 +0200

    Init
    Push all packages

---
+++ com.mathijsbakker.extenject@9.2.0/Source/Install/InstallerBase.cs
@@ -0,0 +1,23 @@
+using System.Threading.Tasks;
+
+namespace Zenject
+{
+    public abstract class InstallerBase : IInstaller
+    {
+        [Inject]
+        DiContainer _container = null;
+
+        protected DiContainer Container
+        {
+            get { return _container; }
+        }
+
+        public virtual bool IsEnabled
+        {
+            get { return true; }
+        }
+
+        public abstract void InstallBindings();
+    }
+}
+
